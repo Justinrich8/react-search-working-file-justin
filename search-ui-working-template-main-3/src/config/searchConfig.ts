4849bd5e6354e71a7f6cd161f7b6f2d3aa049aed
Justinrich8 <109759966+Justinrich8@users.noreply.github.com>
2023-04-06 15:39:55 -0400

    Add files via upload

---
+++ search-ui-working-template-main-3/src/config/searchConfig.ts
@@ -0,0 +1,7 @@
+export const config = {
+  apiKey: "13d214193ff8d3ab3791b25296063512",
+  experienceKey: "ssa-search",
+  locale: "en",
+  experienceVersion: "STAGING/PRODUCTION",
+  businessId: 3157362,
+};
---
+++ search-ui-working-template-main-3/src/pages/ProductsPage.tsx
@@ -0,0 +1,41 @@
+import { useSearchActions } from "@yext/search-headless-react";
+import {
+  AppliedFilters,
+  ResultsCount,
+  SearchBar,
+  StandardCard,
+  VerticalResults,
+  LocationBias,
+  NumericalFacets,
+  Pagination,
+} from "@yext/search-ui-react";
+import { useLayoutEffect } from "react";
+import { CustomCard } from "../components/CustomCard";
+
+export function ProductsPage() {
+  const searchActions = useSearchActions();
+  useLayoutEffect(() => {
+    searchActions.setVertical("products");
+    searchActions.executeVerticalQuery();
+  });
+
+  return (
+    <div>
+      <SearchBar />
+      <div className="flex">
+        <div className="w-56 shrink-0 mr-5">
+          <NumericalFacets />
+        </div>
+        <div className="flex-grow">
+          <div className="flex items-baseline">
+            <ResultsCount />
+            <AppliedFilters />
+          </div>
+          <VerticalResults CardComponent={StandardCard} />
+          <Pagination />
+          <LocationBias />
+        </div>
+      </div>
+    </div>
+  );
+}
